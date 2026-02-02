2e5293d3344e9b3a57f7a7c46fcd7f71e71d3573
unknown <recaldedi7@gmail.com>
2026-02-02 13:18:08 -0300

    feat: Se agrega entidad Sabotage con nombre, posición y estado

--- entities/sabotage.py
+++ entities/sabotage.py
@@ -6,3 +6,9 @@ Responsabilidad:
 - Indicar si están activos o no
 - Proveer efectos al sistema de sabotajes
 """
+class Sabotage:
+ def __init__(self, name, x, y, active=False):
+    self.name = name
+    self.x = x
+    self.y = y
+    self.active = active
