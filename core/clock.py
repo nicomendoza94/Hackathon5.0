e5ae9e72b3222c471db4789d130f4ba13fa5df29
Martin Portillo <maiporti@gmail.com>
2026-02-01 21:26:41 -0300

    feat: agragar funcionalidad para control del tiempo en el juego

--- core/clock.py
+++ core/clock.py
@@ -8,3 +8,12 @@ Responsabilidad:
 
 No debe contener lógica del juego.
 """
+import pygame
+
+class Clock():
+    def __init__(self, fps):
+        self.clock = pygame.time.Clock()
+        self.fps = fps
+    
+    def tick(self):
+        return self.clock.tick(self.fps)
