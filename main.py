060def03c297e0df760d2fc59c00d13e177cc4af
Mathias Ramirez <ramirezmath885@gmail.com>
2026-02-02 18:00:01 -0300

    fix:Correccion main

--- main.py
+++ main.py
@@ -10,25 +10,3 @@ Responsabilidad:
 Este archivo NO debe contener lógica del juego.
 """
 
-import json
-from entities.npc import NPC
-
-def load_npcs(path="data/npcs.json"):
-    """
-    Carga NPCs desde un archivo JSON
-    :param path: ruta al JSON
-    :return: lista de objetos NPC
-    """
-    with open(path, "r", encoding="utf-8") as file:
-        data = json.load(file)
-
-    return [
-        NPC(
-            npc_id=n["id"],
-            name=n["name"],
-            x=n["x"],
-            y=n["y"],
-            is_impostor=n["is_impostor"]
-        )
-        for n in data
-    ]
