45da3256491ecbbd31614182240769bf09a57a2f
Mathias Ramirez <ramirezmath885@gmail.com>
2026-02-02 13:07:31 -0300

    feat: Agrega clase NPC con estado e impostor aleatorio

--- entities/npc.py
+++ entities/npc.py
@@ -6,3 +6,16 @@ Responsabilidad:
 - Mantener su estado interno (trabajando, caminando, sospechoso)
 - Ejecutar comportamientos simples controlados por sistemas
 """
+import random 
+ 
+class NPC: 
+    def __init__(self, name, x, y): 
+        self.name = name 
+        self.x = x 
+        self.y = y 
+        self.state = "WALKING" 
+        self.is_impostor = random.random() < 0.1 
+ 
+    def update(self): 
+        # IA simple 
+        pass 
