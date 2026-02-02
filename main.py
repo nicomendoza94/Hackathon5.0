0ce428f8a33e53b8fcde966eb642348b488264c1
Martin Portillo <maiporti@gmail.com>
2026-02-02 13:13:16 -0300

    feat: Agregar funcionalidad base de loop principal

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ Responsabilidad:
 
 Este archivo NO debe contener lógica del juego.
 """
+import pygame 
+from core.game import Game 
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, TITLE 
+def main(): 
+    pygame.init() 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption(TITLE) 
+    game = Game(screen) 
+    game.run() 
+    pygame.quit() 
+if __name__ == "__main__": 
+    main() 
