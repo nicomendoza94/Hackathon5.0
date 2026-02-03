083fff30e2b3ef93f42b3b04237892d147dcc498
Martin Portillo <maiporti@gmail.com>
2026-02-03 09:36:21 -0300

    feat: Agregar logica de main loop limpio

--- main.py
+++ main.py
@@ -9,4 +9,18 @@ Responsabilidad:
 
 Este archivo NO debe contener lógica del juego.
 """
+import pygame 
+import json
+from core.game import Game 
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, TITLE 
+from entities.npc import NPC
 
+def main(): 
+    pygame.init() 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption(TITLE) 
+    game = Game(screen) 
+    game.run() 
+    pygame.quit() 
+if __name__ == "__main__": 
+    main() 
