5401a723c4d92854a3f1c6629cf1268eededb403
Mathias Ramirez <ramirezmath885@gmail.com>
2026-02-02 23:01:29 -0300

    feat:Se creo la funcion load_npcs.py

---
+++ entities/load_npcs.py
@@ -0,0 +1,18 @@
+import json
+from entities.npc import NPC
+
+def load_npcs(path="data/npcs.json"):
+ 
+    with open(path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return [
+        NPC(
+            npc_id=n["id"],
+            name=n["name"],
+            x=n["x"],
+            y=n["y"],
+            is_impostor=n["is_impostor"]
+        )
+        for n in data
+    ]
